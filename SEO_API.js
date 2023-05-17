1def9a114c51c925dd8eef1bd2171389aef7da4b
kinglearned <kinglearned@gmail.com>
2023-05-17 15:20:20 +0100

    Integrating the parts together

--- SEO_API.js
+++ SEO_API.js
@@ -5,7 +5,7 @@ const SEO_API = (DataBase, SearchData) => {
         for(MainKey in DataBase){
             for (let i = 0; i < DataBase[MainKey].length; i++) {
                 for( SubKey in DataBase[MainKey][i]){// Checking for the sub keys in the object
-                    if(DataBase[MainKey][i][SubKey].toLocaleLowerCase().includes(SearchData.toLocaleLowerCase()) && SearchData !== ''){
+                    if(DataBase[MainKey][i][SubKey].toLocaleLowerCase().includes(SearchData.toLocaleLowerCase()) /*&& SearchData !== ''*/){
                         let index = SearchResult.findIndex(obj => obj.name === DataBase[MainKey][i].name) // To check if the object is already existing in the searchresult
                         const PushObj = index === -1 ? SearchResult.push(DataBase[MainKey][i]) : '' //If Obj dosen't exist Push it to the Result
                     }
@@ -17,7 +17,7 @@ const SEO_API = (DataBase, SearchData) => {
         DataBase.forEach(EachElement => {
             for(SubKey in EachElement){
                 
-                if(EachElement[SubKey].toLocaleLowerCase().includes(SearchData.toLocaleLowerCase()) && SearchData !== ''){
+                if(EachElement[SubKey].toLocaleLowerCase().includes(SearchData.toLocaleLowerCase()) /*&& SearchData !== ''*/){
                     let index = SearchResult.findIndex(obj => obj.name === EachElement.name) // To check if the object is already existing in the searchresult
                     const PushObj = index === -1 ? SearchResult.push(EachElement) : '' //If Obj dosen't exist Push it to the Result
                 }
@@ -28,5 +28,5 @@ const SEO_API = (DataBase, SearchData) => {
     return SearchResult
 }
 
-const TestModule = SEO_API(Obj_Of_An_Arr_Of_Objs, '3000')
+const TestModule = SEO_API(Obj_Of_An_Arr_Of_Objs, QS.value)
 console.log(TestModule)
