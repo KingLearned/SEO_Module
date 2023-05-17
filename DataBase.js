ca30363372f0f48fcda5fdee50befdbbd88be894
kinglearned <kinglearned@gmail.com>
2023-05-17 13:15:36 +0100

    First commit of the Development

---
+++ DataBase.js
@@ -0,0 +1,31 @@
+export const Obj_Of_An_Arr_Of_Objs = {
+    Drinks:[
+        {name:'Ballantines', disc:'A range of blended Scotch whiskies produced by Pernod Ricard in Dumbarton', prc:'35000'},
+        {name:'Coffee Service', disc:'Includes coffee buffet station with all the accompliments (sugar, cream, syrup)', prc:'18750'},
+        {name:'Soft Drinks', disc:'Includes drinks such as Coke, Spirit, 7-Up, Teem, Pepsi, Malt', prc:'30000'},
+        {name:'Juice', disc:'Includes Orange juice, apple juice, pineapple juice, cranberry juice', prc:'35000'},
+        {name:'Wines', disc:'Includes red wine, white wine, rose wine, sparkling wine', prc:'75000'}
+    ],
+    Food:[
+        {name:'Caesar Salad', disc:'Sun dried tomatoes, parmesan, rustic garlic croutons with a traditional caesar dressing', prc:'30000'},
+        {name:'Swedish Meatballs', disc:'Includes button mushrooms, white creamy sauce, natural herbs and fine spices', prc:'71250'},
+        {name:'Iced Oysters', disc:'Fresh iced half shell oysters with lemon and pesto, for summer freshness taste', prc:'63750'},
+        {name:'Spanikopita', disc:'Filo triangles stuffed with crumbled feta & baby spinach baked to a golden brown', prc:'26250'},
+        {name:'Salmon Canape', disc:'Smoked salmon tartines with red onion-caper relish or tender herb mousse', prc:'18750'},
+        {name:'Thai Scallops', disc:'Giant pan seared scallops on skewer. Topped with red curry & basil mango relish', prc:'35000'}
+    ]
+}
+
+export const Arr_Of_Objs = [
+    {name:'Ballantines', disc:'A range of blended Scotch whiskies produced by Pernod Ricard in Dumbarton', prc:'35000'},
+    {name:'Coffee Service', disc:'Includes coffee buffet station with all the accompliments (sugar, cream, syrup)', prc:'18750'},
+    {name:'Soft Drinks', disc:'Includes drinks such as Coke, Spirit, 7-Up, Teem, Pepsi, Malt', prc:'30000'},
+    {name:'Juice', disc:'Includes Orange juice, apple juice, pineapple juice, cranberry juice', prc:'35000'},
+    {name:'Wines', disc:'Includes red wine, white wine, rose wine, sparkling wine', prc:'75000'},
+    {name:'Caesar Salad', disc:'Sun dried tomatoes, parmesan, rustic garlic croutons with a traditional caesar dressing', prc:'30000'},
+    {name:'Swedish Meatballs', disc:'Includes button mushrooms, white creamy sauce, natural herbs and fine spices', prc:'71250'},
+    {name:'Iced Oysters', disc:'Fresh iced half shell oysters with lemon and pesto, for summer freshness taste', prc:'63750'},
+    {name:'Spanikopita', disc:'Filo triangles stuffed with crumbled feta & baby spinach baked to a golden brown', prc:'26250'},
+    {name:'Salmon Canape', disc:'Smoked salmon tartines with red onion-caper relish or tender herb mousse', prc:'18750'},
+    {name:'Thai Scallops', disc:'Giant pan seared scallops on skewer. Topped with red curry & basil mango relish', prc:'35000'}
+]
--- SEO_API.js
+++ SEO_API.js
@@ -1,34 +1,4 @@
-const Obj_Of_An_Arr_Of_Objs = {
-    Drinks:[
-        {name:'Ballantines', disc:'A range of blended Scotch whiskies produced by Pernod Ricard in Dumbarton', prc:'35000'},
-        {name:'Coffee Service', disc:'Includes coffee buffet station with all the accompliments (sugar, cream, syrup)', prc:'18750'},
-        {name:'Soft Drinks', disc:'Includes drinks such as Coke, Spirit, 7-Up, Teem, Pepsi, Malt', prc:'30000'},
-        {name:'Juice', disc:'Includes Orange juice, apple juice, pineapple juice, cranberry juice', prc:'35000'},
-        {name:'Wines', disc:'Includes red wine, white wine, rose wine, sparkling wine', prc:'75000'}
-    ],
-    Food:[
-        {name:'Caesar Salad', disc:'Sun dried tomatoes, parmesan, rustic garlic croutons with a traditional caesar dressing', prc:'30000'},
-        {name:'Swedish Meatballs', disc:'Includes button mushrooms, white creamy sauce, natural herbs and fine spices', prc:'71250'},
-        {name:'Iced Oysters', disc:'Fresh iced half shell oysters with lemon and pesto, for summer freshness taste', prc:'63750'},
-        {name:'Spanikopita', disc:'Filo triangles stuffed with crumbled feta & baby spinach baked to a golden brown', prc:'26250'},
-        {name:'Salmon Canape', disc:'Smoked salmon tartines with red onion-caper relish or tender herb mousse', prc:'18750'},
-        {name:'Thai Scallops', disc:'Giant pan seared scallops on skewer. Topped with red curry & basil mango relish', prc:'35000'}
-    ]
-}
-
-const Arr_Of_Objs = [
-    {name:'Ballantines', disc:'A range of blended Scotch whiskies produced by Pernod Ricard in Dumbarton', prc:'35000'},
-    {name:'Coffee Service', disc:'Includes coffee buffet station with all the accompliments (sugar, cream, syrup)', prc:'18750'},
-    {name:'Soft Drinks', disc:'Includes drinks such as Coke, Spirit, 7-Up, Teem, Pepsi, Malt', prc:'30000'},
-    {name:'Juice', disc:'Includes Orange juice, apple juice, pineapple juice, cranberry juice', prc:'35000'},
-    {name:'Wines', disc:'Includes red wine, white wine, rose wine, sparkling wine', prc:'75000'},
-    {name:'Caesar Salad', disc:'Sun dried tomatoes, parmesan, rustic garlic croutons with a traditional caesar dressing', prc:'30000'},
-    {name:'Swedish Meatballs', disc:'Includes button mushrooms, white creamy sauce, natural herbs and fine spices', prc:'71250'},
-    {name:'Iced Oysters', disc:'Fresh iced half shell oysters with lemon and pesto, for summer freshness taste', prc:'63750'},
-    {name:'Spanikopita', disc:'Filo triangles stuffed with crumbled feta & baby spinach baked to a golden brown', prc:'26250'},
-    {name:'Salmon Canape', disc:'Smoked salmon tartines with red onion-caper relish or tender herb mousse', prc:'18750'},
-    {name:'Thai Scallops', disc:'Giant pan seared scallops on skewer. Topped with red curry & basil mango relish', prc:'35000'}
-]
+import { Obj_Of_An_Arr_Of_Objs, Arr_Of_Objs } from './DataBase'
 
 const SEO_API = (DataBase, SearchData) => {
     const SearchResult = []
@@ -61,3 +31,5 @@ const SEO_API = (DataBase, SearchData) => {
 
     return SearchResult
 }
+
+const TestModule = SEO_API(Obj_Of_An_Arr_Of_Objs, '350000');
